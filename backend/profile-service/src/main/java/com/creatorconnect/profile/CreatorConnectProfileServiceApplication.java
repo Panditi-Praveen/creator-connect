9480c41bb474bf1965f1a5d383e8ac2ce42612f4
Panditi-Praveen <panditipraveen467@gmail.com>
2026-08-08 21:38:50 +0530

    feat(hiring-service): implement Hiring Service with applications, JWT security and tests

--- backend/profile-service/src/main/java/com/creatorconnect/profile/CreatorConnectProfileServiceApplication.java
+++ backend/profile-service/src/main/java/com/creatorconnect/profile/CreatorConnectProfileServiceApplication.java
@@ -18,7 +18,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CreatorConnectProfileServiceApplication {
+public class    CreatorConnectProfileServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreatorConnectProfileServiceApplication.class, args);
